f81ba5eb83db233ad73bf0110ae975b06e1ee5c6
Serhii_Timchak <freemoffice@gmail.com>
2020-07-28 14:32:20 +0300

    FE part connected to the backend.

--- src/user/containers/Auth.js
+++ src/user/containers/Auth.js
@@ -77,17 +77,15 @@ const Auth = () => {
       } catch (error) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
         const responceData = await sendRequest(
           "http://localhost:5000/api/users/signup",
           "POST",
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            "Content-Type": "application/json",
-          }
+          formData
         );
         auth.login(responceData.user.id);
       } catch (error) {}
